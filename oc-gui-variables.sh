6244fe21bd78b2be4c92c089f673034301c0b6b3
catupeloco <catupeloco@gmail.com>
2026-01-18 13:43:11 -0300

    option for rsa in sshkey

--- oc-gui-variables.sh
+++ oc-gui-variables.sh
@@ -3,13 +3,15 @@ export JUMPSERVER=#JUMPSERVER#
 export DNSs='#IPDNSVPN1# #IPDNSVPN2#'
 export VPNNET=#SUBNETVPN#
 export DESTINYS='#SUBNETTARGET1# #SUBNETTARGET2# #SUBNETTARGET3# #SUBNETTARGET4# #HOSTTARGET1# #HOSTTARGET2#'
-export SSH_KEY="-i $HOME/.ssh/#SSHKEY#"
+
 # IF JUMPSERVER ONLY SUPPORTS RSA/DSA
-#export SSH=$(dirname ${0})/openssh-client_9.2p1-2+deb12u7_amd64/ssh
-#export SSH_COPY_ID=$(dirname ${0})/openssh-client_9.2p1-2+deb12u7_amd64/ssh-copy-id
+	#export SSH=$(dirname ${0})/openssh-client_9.2p1-2+deb12u7_amd64/ssh
+	#export SSH_COPY_ID=$(dirname ${0})/openssh-client_9.2p1-2+deb12u7_amd64/ssh-copy-id
+	#export SSH_KEY="-o HostKeyAlgorithms=+ssh-rsa,ssh-dss -o PubkeyAcceptedAlgorithms=+ssh-rsa,ssh-dss -i $HOME/.ssh/id_dsa"
 # FOR EVERY OTHER CASE
-export SSH=/usr/bin/ssh
-export SSH_COPY_ID=/usr/bin/ssh-copy-id
+	export SSH=/usr/bin/ssh
+	export SSH_COPY_ID=/usr/bin/ssh-copy-id
+	export SSH_KEY="-i $HOME/.ssh/#SSHKEY#"
 
 tunnel_hosts=""
 tunnel_hosts+=("#NONREACHABLEHOST1#"	"#REMOTEPORT1#"	"#LOCALPORT1#")
