0a8c30fa1981f133b78bd6b44b01ad43aaeae75a
catupeloco <catupeloco@gmail.com>
2026-01-18 13:39:28 -0300

    add variables script

---
+++ oc-gui-variables.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+export JUMPSERVER=#JUMPSERVER#
+export DNSs='#IPDNSVPN1# #IPDNSVPN2#'
+export VPNNET=#SUBNETVPN#
+export DESTINYS='#SUBNETTARGET1# #SUBNETTARGET2# #SUBNETTARGET3# #SUBNETTARGET4# #HOSTTARGET1# #HOSTTARGET2#'
+export SSH_KEY="-i $HOME/.ssh/#SSHKEY#"
+# IF JUMPSERVER ONLY SUPPORTS RSA/DSA
+#export SSH=$(dirname ${0})/openssh-client_9.2p1-2+deb12u7_amd64/ssh
+#export SSH_COPY_ID=$(dirname ${0})/openssh-client_9.2p1-2+deb12u7_amd64/ssh-copy-id
+# FOR EVERY OTHER CASE
+export SSH=/usr/bin/ssh
+export SSH_COPY_ID=/usr/bin/ssh-copy-id
+
+tunnel_hosts=""
+tunnel_hosts+=("#NONREACHABLEHOST1#"	"#REMOTEPORT1#"	"#LOCALPORT1#")
+tunnel_hosts+=("#NONREACHABLEHOST2#"	"#REMOTEPORT2#"	"#LOCALPORT2#")
+tunnel_hosts+=("#NONREACHABLEHOST3#"	"#REMOTEPORT3#"	"#LOCALPORT3#")
+tunnel_hosts+=("#NONREACHABLEHOST4#"	"#REMOTEPORT4#"	"#LOCALPORT4#")
+tunnel_hosts+=("#NONREACHABLEHOST5#"	"#REMOTEPORT5#"	"#LOCALPORT5#")
+
+
